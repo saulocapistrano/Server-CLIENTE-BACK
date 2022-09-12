5546daf9a2195b2df6c4f25a837998669900b178
Saulo Capistrano <saulocapistrano@gmail.com>
2022-09-12 15:22:36 -0300

    Anotações JPA para classes de domínio

--- src/main/java/com/sjnc/servclient/domain/Tecnico.java
+++ src/main/java/com/sjnc/servclient/domain/Tecnico.java
@@ -3,11 +3,19 @@ package com.sjnc.servclient.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa{
+	
+	private static final long serialVersionUID = 1L;
+	@OneToMany(mappedBy ="tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
