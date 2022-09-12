226e9c17828f79203c6e6e9f0ec8ec65f2a620d1
Saulo Capistrano <saulocapistrano@gmail.com>
2022-09-12 11:20:42 -0300

    Projeto iniciado Server-Cliente

---
+++ src/main/java/com/sjnc/servclient/ServerClienteApplication.java
@@ -0,0 +1,13 @@
+package com.sjnc.servclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerClienteApplication.class, args);
+	}
+
+}
