7e0a9810be48dac26304b76d90e7effb7537b37c
Saulo Capistrano <saulocapistrano@gmail.com>
2022-09-14 12:44:00 -0300

    Perfil de teste finalizado

--- src/main/java/com/sjnc/servclient/config/TestConfig.java
+++ src/main/java/com/sjnc/servclient/config/TestConfig.java
@@ -1,10 +1,23 @@
 package com.sjnc.servclient.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.sjnc.servclient.services.DBService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
+	// intanciando o db service
+	@Autowired
+	private DBService dbService;
+	
+	//método para chamar o DBService de forma a instanciar o banco de dados para teste, necessita da anotation @bean
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
 }
--- src/main/java/com/sjnc/servclient/services/DBService.java
+++ src/main/java/com/sjnc/servclient/services/DBService.java
@@ -1,8 +1,43 @@
 package com.sjnc.servclient.services;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sjnc.servclient.domain.Chamado;
+import com.sjnc.servclient.domain.Cliente;
+import com.sjnc.servclient.domain.Tecnico;
+import com.sjnc.servclient.domain.enums.Perfil;
+import com.sjnc.servclient.domain.enums.Prioridade;
+import com.sjnc.servclient.domain.enums.Status;
+import com.sjnc.servclient.repositories.ChamadoRepository;
+import com.sjnc.servclient.repositories.ClienteRepository;
+import com.sjnc.servclient.repositories.TecnicoRepository;
 
 @Service
 public class DBService {
-
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		
+		Tecnico tec1 = new Tecnico(null, "Saulo ", "15293343572", "saulo@email.com", "123");
+		tec1.addPerfis(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "56468482100", "linos@email.com", "123");
+		// como o perfil de cliente é gerado em qualquer instância, então não é preciso indicar isso neste momento
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+	
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+	
 }
